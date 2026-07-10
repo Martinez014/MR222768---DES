afaad865566ab29fdefc23fffc545c40f33b002d
Martinez014 <edmilson.alejandro12@gmail.com>
2026-07-10 00:03:22 -0600

    Agregar Guía 2

---
+++ Guia 2/MVCPeliculas/MVCPeliculas/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCPeliculas.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string nombre, string apellido, int numVeces = 1)
+        {
+            ViewData["nombre"] = "Hola " + nombre + " " + apellido + ", veces mostrado = " + numVeces;
+            ViewData["numVeces"] = numVeces;
+
+            return View();
+        }
+
+        public string Greeting(string nombre, string apellido, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {nombre} , {apellido} , ID: {id}");
+        }
+    }
+}
